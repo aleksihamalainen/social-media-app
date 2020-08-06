89b61735e7e1067467478cb671722513fabdbaa6
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2020-08-06 18:30:23 +0300

    Added some boilerplate code

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+    res.send('hello world')
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
