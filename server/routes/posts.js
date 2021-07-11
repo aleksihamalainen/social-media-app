1e8edf89f2c40e5e52ff7457f4f5788fa8ea4c91
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2021-07-11 19:31:20 +0300

    More error handling improvements

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -34,43 +34,37 @@ const getTokenFrom = (request) => {
 };
 
 router.get('/', async (req, res) => {
-  const token = getTokenFrom(req);
   try {
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    if (!token || !decodedToken.id) {
-      res.status(401).send({ error: 'Token missing or invalid' });
-    } else {
-      const posts = await Post.find({}).populate('user');
-      res.json(posts);
-    }
+    const token = getTokenFrom(req);
+    jwt.verify(token, process.env.SECRET);
+    const posts = await Post.find({}).populate('user');
+    res.json(posts);
   } catch (error) {
-    res.status(400).send({ error: 'An error occurred' });
+    res.status(401).send({ error: 'Token missing or invalid' });
   }
 });
 
 router.get('/:id', async (req, res) => {
   try {
     const token = getTokenFrom(req);
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    if (!token || !decodedToken.id) {
-      res.status(401).send({ error: 'Token missing or invalid' });
-    } else {
+    jwt.verify(token, process.env.SECRET);
+    try {
       const post = await Post.findById(req.params.id);
       res.json(post);
+    } catch (error) {
+      res.status(404).send({ error: 'Post not found' });
     }
   } catch (error) {
-    res.status(404).send({ error: 'Post not found' });
+    res.status(401).send({ error: 'Token missing or invalid' });
   }
 });
 
 router.post('/', upload.single('postImage'), async (req, res) => {
-  const token = getTokenFrom(req);
   try {
+    const token = getTokenFrom(req);
     const decodedToken = jwt.verify(token, process.env.SECRET);
-    if (!token || !decodedToken.id) {
-      res.status(401).send({ error: 'Token missing or invalid' });
-    } else {
-      const user = await User.findById(decodedToken.id);
+    const user = await User.findById(decodedToken.id);
+    try {
       const post = new Post({
         content: req.body.content,
         user: user._id,
@@ -80,9 +74,11 @@ router.post('/', upload.single('postImage'), async (req, res) => {
       user.posts = user.posts.concat(savedPost._id);
       await user.save();
       res.json(savedPost);
+    } catch (error) {
+      res.status(400).send({ error: 'Invalid parameters' });
     }
   } catch (error) {
-    res.status(400).send({ error: 'An error occurred' });
+    res.status(401).send({ error: 'Token missing or invalid' });
   }
 });
 
@@ -90,20 +86,22 @@ router.delete('/:id', async (req, res) => {
   try {
     const token = getTokenFrom(req);
     const decodedToken = jwt.verify(token, process.env.SECRET);
-    if (!token || !decodedToken.id) {
-      res.status(401).send({ error: 'Token missing or invalid' });
-    } else {
-      const user = await User.findById(decodedToken.id);
-      const post = await Post.findById(req.params.id);
-      if (user._id.toString() === post.user._id.toString()) {
-        await post.remove();
+    const user = await User.findById(decodedToken.id);
+    console.log(user);
+    try {
+      const removedPost = await Post.findById(req.params.id);
+      if (user._id.toString() === removedPost.user._id.toString()) {
+        await removedPost.remove();
+        user.posts = user.posts.filter((post) => post._id !== removedPost._id);
         res.status(204).end();
       } else {
         res.status(403).send({ error: 'Unauthorized' });
       }
+    } catch (error) {
+      res.status(404).send({ error: 'Post not found' });
     }
   } catch (error) {
-    res.status(404).send({ error: 'Post not found' });
+    res.status(401).send({ error: 'Token missing or invalid' });
   }
 });
 
@@ -112,18 +110,22 @@ router.post('/:id/likes', async (req, res) => {
     const token = getTokenFrom(req);
     const decodedToken = jwt.verify(token, process.env.SECRET);
     const user = await User.findById(decodedToken.id);
-    const post = await Post.findById(req.params.id);
-    if (post.likers.includes(user._id)) {
-      res.status(400).send({ error: 'You have liked this post already' });
-    } else {
-      await post.updateOne({
-        $inc: { likes: 1 },
-        $push: { likers: user },
-      });
-      res.status(200).end();
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.likers.includes(user._id)) {
+        res.status(400).send({ error: 'You have liked this post already' });
+      } else {
+        await post.updateOne({
+          $inc: { likes: 1 },
+          $push: { likers: user },
+        });
+        res.status(200).end();
+      }
+    } catch (error) {
+      res.status(404).send({ error: 'Post not found' });
     }
   } catch (error) {
-    res.status(400).send({ error: 'Post not found or token missing' });
+    res.status(401).send({ error: 'Token missing or invalid' });
   }
 });
 
@@ -132,18 +134,22 @@ router.delete('/:id/likes', async (req, res) => {
     const token = getTokenFrom(req);
     const decodedToken = jwt.verify(token, process.env.SECRET);
     const user = await User.findById(decodedToken.id);
-    const post = await Post.findById(req.params.id);
-    if (post.likers.includes(user._id)) {
-      await post.updateOne({
-        $inc: { likes: -1 },
-        $pull: { likers: user._id },
-      });
-    } else {
-      res.status(400).send({ error: 'You have not liked this post' });
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.likers.includes(user._id)) {
+        await post.updateOne({
+          $inc: { likes: -1 },
+          $pull: { likers: user._id },
+        });
+      } else {
+        res.status(400).send({ error: 'You have not liked this post' });
+      }
+      res.status(200).end();
+    } catch (error) {
+      res.status(404).send({ error: 'Post not found' });
     }
-    res.status(200).end();
   } catch (error) {
-    res.status(400).send({ error: 'Post not found or token missing' });
+    res.status(401).send({ error: 'Token missing or invalid' });
   }
 });
 
